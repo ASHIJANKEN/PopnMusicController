a2beec7c0e4789b74248813545995ed4ba057311
ASHIJANKEN <24617794+ASHIJANKEN@users.noreply.github.com>
2018-12-09 22:04:41 +0900

    Updated Control

--- Control/Control.ino
+++ Control/Control.ino
@@ -1,32 +1,36 @@
+/*
+ * Please install the library before compile this.
+ * https://github.com/MHeironimus/ArduinoJoystickLibrary
+ */
+
 #include <Joystick.h>
 
 #define BUTTONS_NUM 9
 
 Joystick_ Joystick;
-int pin_map[BUTTONS_NUM] = {2, 3, 4, 5, 6, 21, 20, 19, 18};
+int pin_map[BUTTONS_NUM] = {21, 2, 19, 3, 15, 5, 16, 7, 9};
 
-void setup() {
-  // Initialize button pins
-  for (int i = 0; i < BUTTONS_NUM; i++){{
-  	pinMode(pin_map[i],INPUT_PULLUP);
-  }
+void setup(){
+	// Initialize button pins
+	for(int i = 0; i < BUTTONS_NUM; i++){
+		pinMode(pin_map[i], INPUT);
+	}
 
-  // Initialize joystick library
-  Joystick.begin();
+	// Initialize joystick library
+	Joystick.begin();
 }
 
 int lastButtonState[BUTTONS_NUM] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-void loop() {
-  // Read pin values
-  for (int i = 0; i < BUTTONS_NUM; i++){
-    int currentButtonState = !digitalRead(pin_map[i]);
-    if (currentButtonState != lastButtonState[i])
-    {
-      Joystick.setButton(i, currentButtonState);
-      lastButtonState[i] = currentButtonState;
-    }
-  }
-  delay(50);
+void loop(){
+	// Read pin values
+	for(int i = 0; i < BUTTONS_NUM; i++){
+		int currentButtonState = !digitalRead(pin_map[i]);
+		if(currentButtonState != lastButtonState[i]){
+			Joystick.setButton(i, currentButtonState);
+			lastButtonState[i] = currentButtonState;
+		}
+	}
+	delay(50);
 }
 
